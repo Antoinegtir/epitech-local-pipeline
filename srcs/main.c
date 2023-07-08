33ff5a2d464d85c76e23f015a43792d80ee3bc2b
Antoine Gonthier <antoine.gonthier@epitech.eu>
2023-07-08 17:06:14 +0200

    ✨ add template srcs C file

---
+++ srcs/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** local_pipeline
+** File description:
+** programme test
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("local pipeline");
+    return 0;
+}
